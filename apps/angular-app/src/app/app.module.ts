c97c2f988c904a8513665bd19fa8bba5268cc3cf
zchapple <zack_chapple@ultimatesoftware.com>
2018-07-31 12:51:26 -0400

    feat(ionic): adding ionic

--- apps/angular-app/src/app/app.module.ts
+++ apps/angular-app/src/app/app.module.ts
@@ -1,13 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { NxModule } from '@nrwl/nx';
+import {AppComponent} from './app.component';
+import {NxModule} from '@nrwl/nx';
+import {MatButtonModule, MatCardModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+const MATERIAL = [MatCardModule, MatButtonModule];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NxModule.forRoot()],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    NxModule.forRoot(),
+    ...MATERIAL
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
